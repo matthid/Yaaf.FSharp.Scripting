e061db2ef8eee16d84262bc783a6725e60feec96
Matthias Dittrich <matthi.d@googlemail.com>
2015-04-25 11:31:54 +0200

    Bump version to 1.0.10

--- src/SharedAssemblyInfo.fs
+++ src/SharedAssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("Yaaf.FSharp.Scripting")>]
 [<assembly: AssemblyProductAttribute("Yaaf.FSharp.Scripting")>]
 [<assembly: AssemblyCopyrightAttribute("Yaaf.FSharp.Scripting Copyright © Matthias Dittrich 2015")>]
-[<assembly: AssemblyVersionAttribute("1.0.8")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.8")>]
-[<assembly: AssemblyInformationalVersionAttribute("1.0.8")>]
+[<assembly: AssemblyVersionAttribute("1.0.10")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.10")>]
+[<assembly: AssemblyInformationalVersionAttribute("1.0.10")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.8"
+    let [<Literal>] Version = "1.0.10"
